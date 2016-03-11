10b6b93dbc0e32a56228b05e86452def315f38b6
Fagot <RFA3456@viseo.net>
2016-03-11 15:42:28 +0100

    update

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -4,7 +4,7 @@
 
  angular.module('myapp')
       .controller('HelloController', function($scope) {
-       $scope.title = 'World, look if your mobile phone provider interfere with HTML code.';
+       $scope.title = 'World, look if your mobile phone provider interfere with HTML code.<br/> If the first image display "Oups" you are parasitised.';
     } );
 
 }) (window.angular);
